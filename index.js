e0b0ea30b3f169f8aeb26f9136e77afda3d5326c
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-05-16 03:29:46 +0530

    Timer Cricket Game with rules

--- index.js
+++ index.js
@@ -34,8 +34,34 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+//******************Custom Alert box from internet********************************** */
+function CustomAlert() {
+    this.render = function () {
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = String(winH) + "px";
+        dialogbox.style.left = String((winW / 2) - (550 * .5)) + "px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxfoot').innerHTML += '<button class="btn btn-success" onclick="Alert.ok()">Yeah!!!</button>';
+    };
+    this.ok = function () {
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    };
+}
+var Alert = new CustomAlert();
+//*****************************************************************************/
 var noOfBalls = 0;
 var noOfPlayers = 10;
+// let possibleRun=[1, 6, 2, 0, 4]
+// let run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
+var possibleRun = [0, 1, 2, 4, 6];
+var runIndex = 0;
+var run = possibleRun[runIndex];
 var game = /** @class */ (function () {
     function game() {
         this.teams = [];
@@ -70,12 +96,14 @@ var hit1 = 1;
 var hit2 = 0;
 var gen = 0;
 var secondsToCountDown = 60;
+// var startup = new Audio('startup.mp3');
+var warning = new Audio("warning.mp3");
 //************************Onload**************************************************
 window.onload = function () {
     localStorage.clear();
     var container = document.createElement("div");
     container.setAttribute("class", "container");
-    container.innerHTML = "\n    <div class=\"row text-center\"><div class=\"col-lg-12\"><button class=\"btn btn-dark\" onclick=\"startTimer()\" id=\"start\">Start the game</button></div></div>\n    <hr>\n    <div class=\"row text-center\">\n        <div class=\"col-4 col-lg-4 text-center\">\n            <h2>Team-1</h2>\n            <h3 id=\"team1Score\">0</h3>\n            <button class=\"btn btn-dark\" id=\"hit1\" onclick=\"hit(1)\" disabled>HIT 1</button>\n        </div>\n        <div class=\"col-4 col-lg-4 text-center\" id=\"timerText\">\n            <h2 >Timer</h2>\n            <h3 id=\"timer\">60</h3>\n        </div>\n        <div class=\"col-4 col-lg-4 text-center\">\n            <h2>Team-2</h2>\n            <h3 id=\"team2Score\">0</h3>\n            <button class=\"btn btn-dark\" id=\"hit2\" onclick=\"hit(2)\" disabled>HIT 2</button>\n        </div>\n    </div>\n    <hr>\n    ";
+    container.innerHTML = "\n    <div class=\"row text-center\">\n    <div class=\"col-4 col-lg-4 text-left\" style=\"font-size:1.3em;\"><p id=\"score1\">Team-1 Score for each Ball goes here</p></div>\n    <div class=\"col-4 col-lg-4 text-center\">\n    <button class=\"btn btn-dark\" onclick=\"startTimer();\" id=\"start\">Start the game</button>\n    <h3 id=\"notifi\"><h3>\n    </div>\n    <div class=\"col-4 col-lg-4 text-right\" style=\"font-size:1.3em;\"><p id=\"score2\">Team-2 Score for each Ball goes here</p></div>\n    </div>\n    <hr>\n    <div class=\"row text-center\">\n        <div class=\"col-4 col-lg-4 text-center\">\n            <h2>Team-1</h2>\n            <h3 id=\"team1Score\">0</h3>\n            <button class=\"btn btn-dark\" id=\"hit1\" onclick=\"hit(1)\" disabled>HIT 1</button>\n        </div>\n        <div class=\"col-4 col-lg-4 text-center\">\n          <h2 id=\"ranRun\">Hit the value here to score</h2>\n            <h1 class=\"timerText\" >Timer</h1>\n            <h3 id=\"timer\" class=\"timerText \">60</h3>\n          \n        </div>\n        <div class=\"col-4 col-lg-4 text-center\">\n            <h2>Team-2</h2>\n            <h3 id=\"team2Score\">0</h3>\n            <button class=\"btn btn-dark\" id=\"hit2\" onclick=\"hit(2)\" disabled>HIT 2</button>\n        </div>\n    </div>\n    <hr>\n    ";
     var container2 = document.createElement("div");
     container2.setAttribute("class", "container-fluid");
     container2.innerHTML = "<div class=\"row \">\n  <div class=\"col-lg-4 text-left\"></div>\n  <div class=\"col-lg-4 text-center\"><a href=\"result.htm\" target=\"__blank\"><button class=\"btn btn-dark\" id=\"gen\"disabled>Generate Result</button></a></div>\n  <div class=\"col-lg-4 text-right\"></div>\n  </div>";
@@ -94,6 +122,7 @@ window.onload = function () {
     col.innerHTML = "<h2>Team-2:Score Board</h2><table class=\"table table-striped\">\n  <thead>\n    <tr id=\"thead2\">\n      \n    </tr>\n  </thead>\n  <tbody id=\"content2\">\n  </tbody>\n  </table>";
     row.appendChild(col);
     container2.appendChild(row);
+    container2.innerHTML += "<br>";
     document.body.appendChild(container);
     document.body.appendChild(container2);
     genTable(1);
@@ -106,6 +135,7 @@ window.onload = function () {
         var play_2 = new player("Player-" + (i + 1), i, "team-2");
         team2.players.push(play_2);
     }
+    Alert.render();
 };
 //****************************************************************************** */
 function genTable(index) {
@@ -135,17 +165,21 @@ function startTimer() {
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
+                    clearVar = 1;
+                    scoreRunner();
                     secondsToCountDown = 60;
                     noOfBalls = 0;
                     if (hit1 == 1) {
                         hit1 = 0;
                         hit2 = 1;
+                        document.getElementById("notifi").innerHTML = "TEAM-1 Batting";
                         document.getElementById("hit1").disabled = false;
                     }
                     else if (hit2 == 1) {
                         hit1 = 0;
                         hit2 = 0;
                         gen = 1;
+                        document.getElementById("notifi").innerHTML = "TEAM-2 Batting";
                         document.getElementById("hit2").disabled = false;
                     }
                     document.getElementById("start").disabled = true;
@@ -153,30 +187,51 @@ function startTimer() {
                     return [4 /*yield*/, temp];
                 case 1:
                     _a.sent();
-                    document.getElementById("timerText").classList.remove("text-danger");
-                    if (hit1 == 0) {
-                        document.getElementById("hit1").disabled = true;
-                    }
-                    else if (hit2 == 0) {
-                        document.getElementById("hit2").disabled = true;
-                    }
+                    clearVar = 0;
+                    document.getElementsByClassName("timerText")[0].classList.remove("text-danger");
+                    document.getElementsByClassName("timerText")[1].classList.remove("text-danger");
                     document.getElementById("timer").innerHTML = String(60);
                     if (hit2 == 0 && hit1 == 0) {
+                        document.getElementById("notifi").innerHTML =
+                            "Click generate result to see the result";
+                        document.getElementById("score2").innerHTML = "Team-2 Scored<br>" + team2.total + " Runs";
+                        document.getElementById("hit2").disabled = true;
                         document.getElementById("start").disabled = true;
                         document.getElementById("gen").disabled = false;
+                        document.getElementById("gen").classList.add("btn-success");
+                        document.getElementById("gen").classList.remove("btn-dark");
                         localStorage.setItem("result", JSON.stringify(cricket));
                         // console.log(String(document.getElementById("scoreBoard").outerHTML));
                         localStorage.setItem("table", String(document.getElementById("scoreBoard").outerHTML));
                     }
-                    else {
-                        // console.log("hi");
+                    else if (hit1 == 0) {
+                        document.getElementById("score1").innerHTML = "Team-1 Scored<br>" + team1.total + " Runs";
+                        document.getElementById("hit1").disabled = true;
                         document.getElementById("start").disabled = false;
-                        // updateButton();
+                        document.getElementById("hit2").disabled = true;
                     }
                     return [2 /*return*/];
             }
         });
     });
+}
+var clearVar = 1;
+function scoreRunner() {
+    var interval = setInterval(function () {
+        // run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
+        runIndex++;
+        if (runIndex > 4) {
+            runIndex = 0;
+        }
+        run = possibleRun[runIndex];
+        // document.getElementById("ranRun").innerHTML = String(run);
+        document.getElementById("ranRun").innerHTML = "Hit on-><span class=\"text-info\" style=\"font-size:1em;\"> " + run + "</span> to score";
+        if (clearVar === 0) {
+            document.getElementById("ranRun").innerHTML =
+                "Hit the value here to score";
+            clearInterval(interval);
+        }
+    }, 120);
 }
 function timer(resolve, reject) {
     //   let secondsToCountDown = 60;
@@ -188,7 +243,13 @@ function timer(resolve, reject) {
         }
         secondsToCountDown--;
         if (secondsToCountDown < 10) {
-            document.getElementById("timerText").classList.add("text-danger");
+            document
+                .getElementsByClassName("timerText")[0]
+                .classList.add("text-danger");
+            document
+                .getElementsByClassName("timerText")[1]
+                .classList.add("text-danger");
+            // warning.play();
         }
     }, 1000);
 }
@@ -218,24 +279,29 @@ var playerBatting2 = 0;
 var player2Total = 0;
 var team2score = 0;
 function hit(hitIndex) {
-    updateValue(hitIndex);
-    var possibleRun = [1, 6, 2, 0, 4];
-    var run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
+    // let possibleRun = [1, 6, 2, 0, 4];
+    // let run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
     //   console.log(run);
     switch (hitIndex) {
         case 1:
             if (playerBatting1 < team1.players.length) {
+                if (run == 0) {
+                    document.getElementById("score1").innerHTML = "0<br>Player-" + (playerBatting1 + 1) + " OUT";
+                }
+                else {
+                    document.getElementById("score1").innerHTML = "Player-" + (playerBatting1 + 1) + " Scored<br>" + run;
+                }
                 team1.players[playerBatting1].score.push(run);
                 player1Total += run;
                 team1score += run;
                 team1.total = team1score;
                 // console.log(`B-${playerBatting1+1}${noOfBalls+1}${hitIndex}`);
                 (document.getElementById("B-" + (playerBatting1 + 1) + (noOfBalls + 1) + hitIndex)).innerHTML = String(run);
+                (document.getElementById("total" + (playerBatting1 + 1) + hitIndex)).innerHTML = String(player1Total);
                 noOfBalls++;
                 if (noOfBalls == 6 || run == 0) {
                     team1.players[playerBatting1].total = player1Total;
                     team1.score.push(player1Total);
-                    (document.getElementById("total" + (playerBatting1 + 1) + hitIndex)).innerHTML = String(player1Total);
                     player1Total = 0;
                     noOfBalls = 0;
                     playerBatting1++;
@@ -249,16 +315,22 @@ function hit(hitIndex) {
             break;
         case 2:
             if (playerBatting2 < team1.players.length) {
+                if (run == 0) {
+                    document.getElementById("score2").innerHTML = "0<br>Player-" + (playerBatting2 + 1) + " OUT";
+                }
+                else {
+                    document.getElementById("score2").innerHTML = "Player-" + (playerBatting2 + 1) + " Scored<br>" + run;
+                }
                 team2.players[playerBatting2].score.push(run);
                 player2Total += run;
                 team2score += run;
                 team2.total = team2score;
                 (document.getElementById("B-" + (playerBatting2 + 1) + (noOfBalls + 1) + hitIndex)).innerHTML = String(run);
+                (document.getElementById("total" + (playerBatting2 + 1) + hitIndex)).innerHTML = String(player2Total);
                 noOfBalls++;
                 if (noOfBalls == 6 || run == 0) {
                     team2.players[playerBatting2].total = player2Total;
                     team2.score.push(player2Total);
-                    (document.getElementById("total" + (playerBatting2 + 1) + hitIndex)).innerHTML = String(player2Total);
                     player2Total = 0;
                     noOfBalls = 0;
                     playerBatting2++;
@@ -271,6 +343,7 @@ function hit(hitIndex) {
             }
             break;
     }
+    updateValue(hitIndex);
 }
 function updateValue(hitIndex) {
     //   console.log("hi");
--- index.ts
+++ index.ts
@@ -1,5 +1,31 @@
+//******************Custom Alert box from internet********************************** */
+function CustomAlert() {
+  this.render = function() {
+      var winW = window.innerWidth;
+      var winH = window.innerHeight;
+      var dialogoverlay = document.getElementById('dialogoverlay');
+      var dialogbox = document.getElementById('dialogbox');
+      dialogoverlay.style.display = "block";
+      dialogoverlay.style.height = String(winH) + "px";
+      dialogbox.style.left = String((winW / 2) - (550 * .5)) + "px";
+      dialogbox.style.top = "100px";
+      dialogbox.style.display = "block";
+      document.getElementById('dialogboxfoot').innerHTML += '<button class="btn btn-success" onclick="Alert.ok()">Yeah!!!</button>';
+  }
+  this.ok = function() {
+      document.getElementById('dialogbox').style.display = "none";
+      document.getElementById('dialogoverlay').style.display = "none";
+  }
+}
+var Alert = new CustomAlert();
+//*****************************************************************************/
 let noOfBalls = 0;
 let noOfPlayers = 10;
+// let possibleRun=[1, 6, 2, 0, 4]
+// let run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
+let possibleRun = [0, 1, 2, 4, 6];
+let runIndex = 0;
+let run = possibleRun[runIndex];
 class game {
   teams: team[] = [];
   winningTeam: team;
@@ -42,13 +68,23 @@ let hit1 = 1;
 let hit2 = 0;
 let gen = 0;
 let secondsToCountDown = 60;
+// var startup = new Audio('startup.mp3');
+var warning = new Audio("warning.mp3");
+
 //************************Onload**************************************************
 window.onload = () => {
-    localStorage.clear();
+  localStorage.clear();
   let container = <HTMLDivElement>document.createElement("div");
   container.setAttribute("class", "container");
   container.innerHTML = `
-    <div class="row text-center"><div class="col-lg-12"><button class="btn btn-dark" onclick="startTimer()" id="start">Start the game</button></div></div>
+    <div class="row text-center">
+    <div class="col-4 col-lg-4 text-left" style="font-size:1.3em;"><p id="score1">Team-1 Score for each Ball goes here</p></div>
+    <div class="col-4 col-lg-4 text-center">
+    <button class="btn btn-dark" onclick="startTimer();" id="start">Start the game</button>
+    <h3 id="notifi"><h3>
+    </div>
+    <div class="col-4 col-lg-4 text-right" style="font-size:1.3em;"><p id="score2">Team-2 Score for each Ball goes here</p></div>
+    </div>
     <hr>
     <div class="row text-center">
         <div class="col-4 col-lg-4 text-center">
@@ -56,9 +92,11 @@ window.onload = () => {
             <h3 id="team1Score">0</h3>
             <button class="btn btn-dark" id="hit1" onclick="hit(1)" disabled>HIT 1</button>
         </div>
-        <div class="col-4 col-lg-4 text-center" id="timerText">
-            <h2 >Timer</h2>
-            <h3 id="timer">60</h3>
+        <div class="col-4 col-lg-4 text-center">
+          <h2 id="ranRun">Hit the value here to score</h2>
+            <h1 class="timerText" >Timer</h1>
+            <h3 id="timer" class="timerText ">60</h3>
+          
         </div>
         <div class="col-4 col-lg-4 text-center">
             <h2>Team-2</h2>
@@ -77,7 +115,7 @@ window.onload = () => {
   </div>`;
   let row = <HTMLDivElement>document.createElement("div");
   row.setAttribute("class", "row");
-  row.id="scoreBoard";
+  row.id = "scoreBoard";
   let col = <HTMLDivElement>document.createElement("div");
   col.setAttribute("class", "col-12 col-lg-5 text-center");
   col.innerHTML = `<h2>Team-1:Score Board</h2><table class="table table-striped">
@@ -107,7 +145,7 @@ window.onload = () => {
   </table>`;
   row.appendChild(col);
   container2.appendChild(row);
-
+container2.innerHTML+="<br>";
   document.body.appendChild(container);
   document.body.appendChild(container2);
   genTable(1);
@@ -121,6 +159,7 @@ window.onload = () => {
     let play = new player(`Player-${i + 1}`, i, "team-2");
     team2.players.push(play);
   }
+  Alert.render();
 };
 //****************************************************************************** */
 
@@ -162,42 +201,77 @@ function genTable(index) {
 // console.log(team2);
 
 async function startTimer() {
+  clearVar = 1;
+  scoreRunner();
   secondsToCountDown = 60;
   noOfBalls = 0;
   if (hit1 == 1) {
     hit1 = 0;
     hit2 = 1;
+    document.getElementById("notifi").innerHTML = "TEAM-1 Batting";
     (<HTMLButtonElement>document.getElementById("hit1")).disabled = false;
   } else if (hit2 == 1) {
     hit1 = 0;
     hit2 = 0;
     gen = 1;
+    document.getElementById("notifi").innerHTML = "TEAM-2 Batting";
     (<HTMLButtonElement>document.getElementById("hit2")).disabled = false;
   }
   (<HTMLButtonElement>document.getElementById("start")).disabled = true;
   let temp = new Promise((resolve, reject) => timer(resolve, reject));
   await temp;
-  document.getElementById("timerText").classList.remove("text-danger");
-  if(hit1==0){
-    (<HTMLButtonElement>document.getElementById("hit1")).disabled = true;
-  }
-  else if(hit2==0){
-    (<HTMLButtonElement>document.getElementById("hit2")).disabled = true;
-  }
+  clearVar = 0;
+  document.getElementsByClassName("timerText")[0].classList.remove("text-danger");
+  document.getElementsByClassName("timerText")[1].classList.remove("text-danger");
   document.getElementById("timer").innerHTML = String(60);
   if (hit2 == 0 && hit1 == 0) {
+    document.getElementById("notifi").innerHTML =
+      "Click generate result to see the result";
+    document.getElementById(
+      "score2"
+    ).innerHTML = `Team-2 Scored<br>${team2.total} Runs`;
+    (<HTMLButtonElement>document.getElementById("hit2")).disabled = true;
     (<HTMLButtonElement>document.getElementById("start")).disabled = true;
     (<HTMLButtonElement>document.getElementById("gen")).disabled = false;
+    (<HTMLButtonElement>document.getElementById("gen")).classList.add(
+      "btn-success"
+    );
+    (<HTMLButtonElement>document.getElementById("gen")).classList.remove(
+      "btn-dark"
+    );
     localStorage.setItem("result", JSON.stringify(cricket));
     // console.log(String(document.getElementById("scoreBoard").outerHTML));
-    localStorage.setItem("table",String(document.getElementById("scoreBoard").outerHTML));
-  } else {
-    // console.log("hi");
+    localStorage.setItem(
+      "table",
+      String(document.getElementById("scoreBoard").outerHTML)
+    );
+  } else if (hit1 == 0) {
+    document.getElementById(
+      "score1"
+    ).innerHTML = `Team-1 Scored<br>${team1.total} Runs`;
+    (<HTMLButtonElement>document.getElementById("hit1")).disabled = true;
     (<HTMLButtonElement>document.getElementById("start")).disabled = false;
-    // updateButton();
+    (<HTMLButtonElement>document.getElementById("hit2")).disabled = true;
   }
 }
-
+let clearVar = 1;
+function scoreRunner() {
+  const interval = setInterval(() => {
+    // run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
+    runIndex++;
+    if (runIndex > 4) {
+      runIndex = 0;
+    }
+    run = possibleRun[runIndex];
+    // document.getElementById("ranRun").innerHTML = String(run);
+    document.getElementById("ranRun").innerHTML =`Hit on-><span class="text-info" style="font-size:1em;"> ${run}</span> to score`;
+    if (clearVar === 0) {
+      document.getElementById("ranRun").innerHTML =
+        "Hit the value here to score";
+      clearInterval(interval);
+    }
+  }, 120);
+}
 function timer(resolve, reject) {
   //   let secondsToCountDown = 60;
   const interval = setInterval(() => {
@@ -207,8 +281,15 @@ function timer(resolve, reject) {
       return resolve("sucess");
     }
     secondsToCountDown--;
+
     if (secondsToCountDown < 10) {
-      document.getElementById("timerText").classList.add("text-danger");
+      document
+        .getElementsByClassName("timerText")[0]
+        .classList.add("text-danger");
+      document
+        .getElementsByClassName("timerText")[1]
+        .classList.add("text-danger");
+      // warning.play();
     }
   }, 1000);
 }
@@ -240,15 +321,22 @@ let playerBatting2 = 0;
 let player2Total = 0;
 let team2score = 0;
 
-
 function hit(hitIndex: number) {
-  updateValue(hitIndex);
-  let possibleRun = [1, 6, 2, 0, 4];
-  let run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
+  // let possibleRun = [1, 6, 2, 0, 4];
+  // let run = possibleRun[Math.floor(Math.random() * possibleRun.length)];
   //   console.log(run);
   switch (hitIndex) {
     case 1:
       if (playerBatting1 < team1.players.length) {
+        if (run == 0) {
+          document.getElementById("score1").innerHTML = `0<br>Player-${
+            playerBatting1 + 1
+          } OUT`;
+        } else {
+          document.getElementById("score1").innerHTML = `Player-${
+            playerBatting1 + 1
+          } Scored<br>${run}`;
+        }
         team1.players[playerBatting1].score.push(run);
         player1Total += run;
         team1score += run;
@@ -259,13 +347,14 @@ function hit(hitIndex: number) {
             `B-${playerBatting1 + 1}${noOfBalls + 1}${hitIndex}`
           )
         )).innerHTML = String(run);
+        (<HTMLTableCellElement>(
+          document.getElementById(`total${playerBatting1 + 1}${hitIndex}`)
+        )).innerHTML = String(player1Total);
         noOfBalls++;
         if (noOfBalls == 6 || run == 0) {
           team1.players[playerBatting1].total = player1Total;
           team1.score.push(player1Total);
-          (<HTMLTableCellElement>(
-            document.getElementById(`total${playerBatting1 + 1}${hitIndex}`)
-          )).innerHTML = String(player1Total);
+
           player1Total = 0;
           noOfBalls = 0;
           playerBatting1++;
@@ -278,6 +367,15 @@ function hit(hitIndex: number) {
       break;
     case 2:
       if (playerBatting2 < team1.players.length) {
+        if (run == 0) {
+          document.getElementById("score2").innerHTML = `0<br>Player-${
+            playerBatting2 + 1
+          } OUT`;
+        } else {
+          document.getElementById("score2").innerHTML = `Player-${
+            playerBatting2 + 1
+          } Scored<br>${run}`;
+        }
         team2.players[playerBatting2].score.push(run);
         player2Total += run;
         team2score += run;
@@ -287,13 +385,13 @@ function hit(hitIndex: number) {
             `B-${playerBatting2 + 1}${noOfBalls + 1}${hitIndex}`
           )
         )).innerHTML = String(run);
+        (<HTMLTableCellElement>(
+          document.getElementById(`total${playerBatting2 + 1}${hitIndex}`)
+        )).innerHTML = String(player2Total);
         noOfBalls++;
         if (noOfBalls == 6 || run == 0) {
           team2.players[playerBatting2].total = player2Total;
           team2.score.push(player2Total);
-          (<HTMLTableCellElement>(
-            document.getElementById(`total${playerBatting2 + 1}${hitIndex}`)
-          )).innerHTML = String(player2Total);
           player2Total = 0;
           noOfBalls = 0;
           playerBatting2++;
@@ -305,6 +403,7 @@ function hit(hitIndex: number) {
       }
       break;
   }
+  updateValue(hitIndex);
 }
 
 function updateValue(hitIndex) {
